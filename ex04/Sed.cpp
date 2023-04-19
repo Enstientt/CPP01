d1760a1b471b67b3451325365e439da10adfaaf6
Enstientt <76008179+Enstientt@users.noreply.github.com>
2023-04-19 05:07:15 +0000

    Add files via upload

--- ex04/Sed.cpp
+++ ex04/Sed.cpp
@@ -6,7 +6,7 @@
 /*   By: zessadqu <zessadqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 20:04:11 by zessadqu          #+#    #+#             */
-/*   Updated: 2023/04/19 01:52:26 by zessadqu         ###   ########.fr       */
+/*   Updated: 2023/04/19 03:14:43 by zessadqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,42 +20,33 @@ Sed::~Sed()
 {
 }
 
-void	Sed::replace(void)
+void Sed::replace(void)
 {
-    std::string  buffer;
-    std::ifstream	ifs(_filename);
-    std::ofstream	ofs(_filename + ".replace");
-    std::string		line;
+    std::ifstream ifs(_filename);
+    std::ofstream ofs(_filename + ".replace");
+    std::string line;
 
     if (!ifs.is_open())
     {
         std::cout << "Error: File not found" << std::endl;
-        return ;
+        return;
     }
-    if (_s1 !="")
+    if (!ofs.is_open())
     {
-        while (std::getline(ifs, line))
-        {
-            while (line.find(_s1) != std::string::npos)
-            {
-                if (line.find(_s1) !=0)
-                {
-                    buffer = line.substr(0, line.find(_s1) );
-                    buffer += _s2;
-                    buffer += line.substr(line.find(_s1) + _s1.length());
-                    line = buffer;
-                }
-                else
-                {
-                    buffer = line.substr(0, line.find(_s1) + 1 );
-                    buffer += _s2;
-                    buffer += line.substr(line.find(_s1) + 1);
-                    line = buffer;
-                }
+        std::cout << "Error : creation of the file failed" << std::endl;
+        return;
+    }
+    if (_s1 != "") {
+        while (std::getline(ifs, line)) {
+            size_t pos = 0;
+            while ((pos = line.find(_s1, pos)) != std::string::npos) {
+                line = line.substr(0, pos) + _s2 + line.substr(pos + _s1.length());
+                pos += _s2.length();
             }
-            ofs << line << std::endl;
+            ofs << line ;
         }
     }
+
     ifs.close();
     ofs.close();
 }
